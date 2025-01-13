72e1af72f0fac59ef50764e1300c6198dd0c44da
simon jimenez <simoncho2002@gmail.com>
2025-01-13 18:24:05 -0300

    usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "eliminando usuarios"
+User.destroy_all
+puts "creando usuarios"
+User.create!(
+  [
+  {email: "admin@gmail.com", password: "291ABASD932"},
+  {email: "usuario1@gmail.com", password: "291ABASD934"},
+  {email: "usuario2@gmail.com", password: "281ABASD932"}
+
+  ]
+)
+puts "usuarios creados exitosamente"
